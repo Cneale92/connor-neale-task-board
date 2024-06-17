677de2755ac3f4a76175b1580fd1be56a0fa3756
Cneale92 <connorneale92@gmail.com>
2024-06-16 20:40:47 -0500

    fixed issues with misspelling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,76 +3,75 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 function readTasksFromLocalStorage() {
-    let tasks = localStorage.getItem("tasks");
-    if(!tasks) {
-        return [];
-    }
-    let tasksParsed = JSON.parse(tasks);
-    return tasksParsed;
+  let tasks = localStorage.getItem("tasks");
+  if (!tasks) {
+    return [];
+  }
+  let tasksParsed = JSON.parse(tasks);
+  return tasksParsed;
 }
 
 function saveTasksToLocalStorage(tasks) {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    return crypto.randomUUID();
+  return crypto.randomUUID();
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const taskCard = $('<div>');
-    taskCard.addClass('card project-card draggable my-3');
-    taskCard.attr('taskcard-id', task.id);
+  const taskCard = $("<div>");
+  taskCard.addClass("card project-card draggable my-3");
+  taskCard.attr("data-project-id", task.id);
 
-    const cardHeader = $('<div>');
-    cardHeader.addClass('card-header h4');
-    cardHeader.text(task.title);
+  const cardHeader = $("<div>");
+  cardHeader.addClass("card-header h4");
+  cardHeader.text(task.title);
 
-    const cardBody = $('<div>');
-    cardBody.addClass('card-body');
+  const cardBody = $("<div>");
+  cardBody.addClass("card-body");
 
-    const cardDate = $('<p>');
-    cardDate.addClass('card-text');
-    cardDate.text(task.dueDate);
+  const cardDate = $("<p>");
+  cardDate.addClass("card-text");
+  cardDate.text(task.dueDate);
 
-    const cardDescription = $('<p>');
-    cardDescription.addClass('card-text');
-    cardDescription.text(task.description);
+  const cardDesc = $("<p>");
+  cardDesc.addClass("card-text");
+  cardDesc.text(task.description);
 
-    const cardDeleteButton = $('<button>');
-    cardDeleteButton.addClass('btn btn-danger delete');
-    cardDeleteButton.text('Delete');
-    cardDeleteButton.attr('taskcard-id', task.id);
+  const cardDeleteButton = $("<button>");
+  cardDeleteButton.addClass("btn btn-danger delete");
+  cardDeleteButton.text("Delete");
+  cardDeleteButton.attr("data-project-id", task.id);
 
-    cardBody.append(cardDate);
-    cardBody.append(cardDesc);
-    cardBody.append(cardDeleteButton);
+  cardBody.append(cardDate);
+  cardBody.append(cardDesc);
+  cardBody.append(cardDeleteButton);
 
-    taskCard.append(cardHeader);
-    taskCard.append(cardBody);
+  taskCard.append(cardHeader);
+  taskCard.append(cardBody);
 
-  if (task.dueDate && task.status !== 'done') {
+  if (task.dueDate && task.status !== "done") {
     const now = dayjs();
-    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    const taskDueDate = dayjs(task.dueDate, "DD/MM/YYYY");
 
-    let sevenDaysAway = now.add(7, 'day');
+    let sevenDaysAway = now.add(7, "day");
     const isBetween = window.dayjs_plugin_isBetween;
     dayjs.extend(isBetween);
 
-    if (dayjs(taskDueDate).isBetween(now, sevenDaysAway, 'day', "[)")) {
-      taskCard.addClass('bg-warning text-white');
-    } else if (now.isAfter(taskDueDate, 'day')) {
-      taskCard.addClass('bg-danger text-white');
-      cardDeleteButton.addClass('border-light');
+    if (dayjs(taskDueDate).isBetween(now, sevenDaysAway, "day", "[)")) {
+      taskCard.addClass("bg-warning text-white");
+    } else if (now.isAfter(taskDueDate, "day")) {
+      taskCard.addClass("bg-danger text-white");
+      cardDeleteButton.addClass("border-light");
     }
+    return taskCard;
+  }
 
   return taskCard;
 }
-  return taskCard;
-}
-
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -97,6 +96,7 @@ function renderTaskList() {
       doneList.append(newCard);
     }
   }
+
   $(".draggable").draggable({
     opacity: 0.5,
     zIndex: 100,
@@ -136,7 +136,6 @@ function handleAddTask(event) {
   $("#taskDescription").val("");
 }
 
-
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {
   const taskId = $(this).attr("data-project-id");
@@ -154,7 +153,7 @@ function handleDeleteTask(event) {
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
   const tasks = readTasksFromLocalStorage();
-  const taskId = ui.draggable[0].dataset.projectId
+  const taskId = ui.draggable[0].dataset.projectId;
   const newStatus = event.target.id;
 
   for (i of tasks) {
@@ -168,23 +167,17 @@ function handleDrop(event, ui) {
 }
 
 $(document).ready(function () {
-
   renderTaskList();
 
-  $('.swim-lanes').on('click', '.delete', handleDeleteTask);
-
-
-  $('#project-form').on('submit', handleAddTask);
-
- 
+  $(".swim-lanes").on("click", ".delete", handleDeleteTask);
+  $("#project-form").on("submit", handleAddTask);
   $(".lane").droppable({
-    accept: '.draggable',
+    accept: ".draggable",
     drop: handleDrop,
   });
 
-    $( "#taskDueDate" ).datepicker({
-        changeMonth: true,
-        changeYear: true
-      });
-
+  $("#taskDueDate").datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
 });
