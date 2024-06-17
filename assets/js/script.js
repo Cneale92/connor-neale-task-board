750a22a3ed0de43d2c564d46ac6755a434376f66
Cneale92 <connorneale92@gmail.com>
2024-06-16 19:41:22 -0500

    fixed spelling errors and added task-button id to button on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,8 +167,6 @@ function handleDrop(event, ui) {
   renderTaskList();
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, 
-// and make the due date field a date picker
 $(document).ready(function () {
 
   renderTaskList();
